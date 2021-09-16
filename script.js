49936625c449224f107f22a011930e208b8feac1
Sara Ward <saro4cka.ward@yandex.com>
2021-09-16 10:59:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// unnecessary as function done in CSS
+
+//const text = document.querySelector(".text");
+//const textInfo = document.querySelector(".text-info");
+
+//function handleEnter() { 
+    //textInfo.style.opacity = 1;
+
+
+//function handleExit() {
+   // textInfo.style.opacity = 0;
+
+
+//text.addEventListener("mouseover", handleEnter);
+
+//text.addEventListener("mouseenter", handleEnter);
+
+//text.addEventListener("mouseout", handleExit);
+
+//text.addEventListener("mouseleave", handleExit);//
